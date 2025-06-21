33de01a2c6d19ab73b14b33addc1f471d55d5c96
Vladimir Bun <qwelp@Mail.ru>
2025-06-21 19:32:57 +0300

    Добавлена поддержка родительских кодов для групп настроек в компоненте и переработана обработка данных в OptionsManager и JavaScript для улучшения функциональности.

--- local/modules/qwelp.site_settings/install/components/qwelp/site.settings/lang/ru/component.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Языковые константы для компонента панели настроек сайта
- * 
- * @package qwelp.site_settings
- */
-
-$MESS['QWELP_SITE_SETTINGS_MODULE_NOT_INSTALLED'] = 'Модуль настроек сайта не установлен';
-$MESS['QWELP_SITE_SETTINGS_PAGE_TITLE'] = 'Настройки сайта';
